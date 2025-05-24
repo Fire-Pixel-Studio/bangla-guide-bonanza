0e9eaf9e20227cac561db2aeb0a26f98f9a25986
Maheed Ahmed <207199307+Fire-Pixel-Studio@users.noreply.github.com>
2025-05-24 19:33:09 +0600

    Add Banner Ads

---
+++ src/components/AdBanner.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const AdBanner = () => {
+  return (
+    <div className="fixed right-4 top-1/2 -translate-y-1/2 z-10 hidden lg:block">
+      <script type="text/javascript">
+        {`
+          atOptions = {
+            'key' : 'd0969275e0774d9286db47226a652637',
+            'format' : 'iframe',
+            'height' : 600,
+            'width' : 160,
+            'params' : {}
+          };
+        `}
+      </script>
+      <script type="text/javascript" src="http://www.profitabledisplaynetwork.com/d0969275e0774d9286db47226a652637/invoke.js"></script>
+    </div>
+  );
+};
+
+export default AdBanner;
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,10 @@
-
 import { useState } from 'react';
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
 import CategoriesSection from '../components/CategoriesSection';
 import Footer from '../components/Footer';
 import Background from '../components/Background';
+import AdBanner from '../components/AdBanner';
 
 const Index = () => {
   const [language, setLanguage] = useState<'en' | 'bn'>('en');
@@ -17,6 +17,7 @@ const Index = () => {
     <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-200 relative">
       <Background />
       <Header language={language} onLanguageChange={handleLanguageChange} />
+      <AdBanner />
       <HeroSection language={language} />
       <CategoriesSection language={language} />
       <Footer language={language} />
